389e59f600f5c3e1217a093e6b69af81c9beaa98
govnotest <47129201+govnotest@users.noreply.github.com>
2026-04-21 17:53:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,9 @@ const define = {
   // TEST: valueToBoolean(process.env.TEST, false),
   // SERVICE: valueToBoolean(process.env.SERVICE, false),
 }
-
+server: {
+  allowedHosts: true
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: ['BRIEFING_', 'APP_', 'VITE_'],
